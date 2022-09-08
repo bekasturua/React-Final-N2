9dd30d1528765ac52bb7edac092f77c439f824e0
Beka Sturua <beqasturua3@gmail.com>
2022-09-08 15:37:52 +0400

    Sending a POST Request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,7 +2,16 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
   function addMeetupHandler(meetupData) {
-    
+    fetch(
+      "https://react-getting-started-217da-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 
   return (
